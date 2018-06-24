c47bbcefbe68a9c41127ef4f4a7b418c34df3251
Saloni <mail.saloniuppal@gmail.com>
2018-06-24 16:01:13 +0200

    changed php ini and some code

--- public/info.php
+++ public/info.php
@@ -6,11 +6,61 @@
  * Time: 12:23 AM
  */
 
-echo "Hello Pakodeloni";
+//echo "Hello Pakodeloni";
 
 //phpinfo();
 
 
+class Family {
+
+    private $members;
+    private $cast;
+
+    public function __construct()
+    {
+        $this->cast = "Uppal";
+        $this->members = 6;
+
+    }
+
+    public function __toString()
+    {
+     return "Saloni is beautiful";
+
+    }
+
+    public function __debugInfo()
+    {
+        return [
+            'propSquared' => 'saloni'
+        ];
+    }
+
+    public function __set_state(array $array)
+    {
+
+        $temp = new Family();
+        $temp->members = $array['members'];
+        $temp->cast = "Our Family name is : " . $array['cast'];
+        return $temp;
+
+    }
+
+    public function __invoke()
+    {
+        echo "I can act now as a function";
+        // TODO: Implement __invoke() method.
+    }
+
+
+
+
+}
+
+$obj = new Family();
+//$code = var_export($obj, true);
+//eval($code . ";");
+//$obj();
 ?>
 
 
